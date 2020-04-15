ec6e544774bbd92d2d5f24eb99bef1c6d4c6e32e
Michael Piercy <michael.piercy@zoho.com>
2020-04-15 22:12:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(n)
+ countdown_output = 10
+ while countdown_output >0
+ puts "#{n} SECOND(S)!"
+ n = countdown_output -= 1
+end
+"HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  countdown_output = 12
+  sleep 5
+  while countdown_output >0
+  puts "#{n} SECOND(S)!"
+  n = countdown_output -= 1
+end
 end
